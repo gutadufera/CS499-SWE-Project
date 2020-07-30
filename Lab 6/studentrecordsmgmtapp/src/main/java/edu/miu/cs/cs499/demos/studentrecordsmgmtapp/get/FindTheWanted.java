776d5c76a25ef77465c8a1baaf542e716844a0bd
gutadufera <53204963+gutadufera@users.noreply.github.com>
2020-07-29 22:32:10 -0500

    lab6

---
+++ Lab 6/studentrecordsmgmtapp/src/main/java/edu/miu/cs/cs499/demos/studentrecordsmgmtapp/get/FindTheWanted.java
@@ -0,0 +1,17 @@
+package edu.miu.cs.cs499.demos.studentrecordsmgmtapp.get;
+
+public class FindTheWanted {
+    public static int findSecondBiggest(int[]arr){
+        int len = arr.length;
+        int temp;
+        for(int i=0; i<len;  i++){
+            for(int j=i+1; j<len; j++){
+                if(arr[i]>arr[j]){
+                    temp=arr[i];
+                    
+                }
+            }
+        }
+
+    }
+}
